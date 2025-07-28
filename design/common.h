38b95936b12c704b5ec25ec89aa736237b134a17
Austin Owens <austin.t.owens@jpl.nasa.gov>
2025-07-27 22:49:32 -0700

    Fix env_setup.sh. Add default common.h and system.cfg vals.

--- design/common.h
+++ design/common.h
@@ -10,11 +10,11 @@
 
 // Number of pulses to process. This will also be the number of rows of the
 // output image.
-#define PULSES 5
+#define PULSES 100
 
 // Number of range compression samples. This will also be the number of columns
 // of the output image.
-#define RC_SAMPLES 64
+#define RC_SAMPLES 512
 
 // Number of azimuth samples. This will also be the number of rows of the
 // output image.
@@ -25,7 +25,7 @@
 
 // Number of image reconstruction solvers per switch. It must be a power of 2
 // and the max number of image solvers on a switch is 32
-#define IMG_SOLVERS_PER_SWITCH 4
+#define IMG_SOLVERS_PER_SWITCH 32
 
 // Number of image reconstruction solvers (must be pwr of 2)
 #define IMG_SOLVERS (AIE_SWITCHES*IMG_SOLVERS_PER_SWITCH)
